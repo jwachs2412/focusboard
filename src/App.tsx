b8f70b636526ce4e8bf385c5ede7445c5afc77d7
Wachsman, Joshua <joshua.wachsman@radancy.com>
2026-03-03 21:35:28 -0500

    added a registration page for user to register a new account

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Layout from "./components/layout/Layout"
 import TaskBoard from "./components/tasks/TaskBoard"
+import RegisterPage from "./pages/RegisterPage"
 import LoginPage from "./pages/LoginPage"
 import { ProtectedRoute } from "../src/components/ProtectedRoute"
 
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         {/* Public Route */}
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
 
         {/* Protected Routes */}
---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react"
+// import { useAuth } from "../contexts/AuthContext"
+import { useNavigate } from "react-router-dom"
+
+const API_URL = import.meta.env.VITE_API_URL
+
+export default function RegisterPage() {
+  //   const { login } = useAuth()
+  const navigate = useNavigate()
+
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState<string | null>(null)
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+
+    const response = await fetch(`${API_URL}/auth/register`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, email, password })
+    })
+
+    const data = await response.json()
+
+    if (!response.ok) {
+      setError(data.error || "Registration failed")
+      return
+    }
+
+    // login(data.token)
+    navigate("/login") // send to login page after registration, user can then login with new credentials
+  }
+
+  return (
+    <div>
+      <h2>Register</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Name" value={name} onChange={e => setName(e.target.value)} />
+        <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
+
+        <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+
+        <button type="submit">Register</button>
+      </form>
+
+      {error && <p>{error}</p>}
+    </div>
+  )
+}
