8a9e5c3642de86b63b3e454689adbc81650a11be
Wachsman, Joshua <jwachs2412@gmail.com>
2026-03-18 16:03:16 -0400

    updated App so that pages lazy load instead of all loading at once (code splitting)

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,35 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Suspense, lazy } from "react"
 import Layout from "./components/layout/Layout"
-import TaskBoard from "./components/tasks/TaskBoard"
-import RegisterPage from "./pages/RegisterPage"
-import LoginPage from "./pages/LoginPage"
 import { ProtectedRoute } from "./components/ProtectedRoute"
+
+// Lazy loaded pages
+const TaskBoard = lazy(() => import("./components/tasks/TaskBoard"))
+const RegisterPage = lazy(() => import("./pages/RegisterPage"))
+const LoginPage = lazy(() => import("./pages/LoginPage"))
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* Public Route */}
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {/* Public Route */}
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
 
-        {/* Protected Routes */}
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Layout>
-                <TaskBoard />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
+          {/* Protected Routes */}
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Layout>
+                  <TaskBoard />
+                </Layout>
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   )
 }
